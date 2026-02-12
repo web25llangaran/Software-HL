63e4d9b19d565fed07f670d230157b46fa368bf5
web25hagirre <web25haguirre@imaltuna.com>
2026-02-12 09:45:55 +0100

    Exekutablea sortzeko behar diren aldaketak

--- app/src/main/java/programa/App.java
+++ app/src/main/java/programa/App.java
@@ -6,7 +6,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 /**
@@ -25,7 +24,7 @@ public class App extends Application {
 
         //Apparen windowsaren itxura aldatu. Hau satgen egin behar da ezin da Scene Builderen egin
         //Logoa gehitu
-        stage.getIcons().add(new Image(getClass().getResourceAsStream("/programa/logoa/miguel-altuna-logo.png")));
+        //stage.getIcons().add(new Image(getClass().getResourceAsStream("/programa/logoa/miguel-altuna-logo.png")));
         //Leihoari izenburua jarri
         stage.setTitle("Maltuna Brand - TxirbilDB");
         //______________________________________________________________
